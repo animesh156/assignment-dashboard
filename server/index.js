8046a8fddab88a7b6f925895780af1137c3f1514
animesh <animeshrathore255@gmail.com>
2025-03-06 19:28:43 +0530

    feat: connected DB to backend

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv').config()
-const port = process.env.PORT
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const port = process.env.PORT;
+var cors = require("cors");
+const connectDB = require("./config/db");
 
+connectDB();
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
-app.get('/', (req,res) => {
-      res.json("hello")
-})
-
+app.get("/", (req, res) => {
+  res.json("hello");
+});
 
 app.listen(port, () => {
-    console.log(`server started at ${port}`)
-})
+  console.log(`server started at ${port}`);
+});
